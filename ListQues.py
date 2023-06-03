ce0c87991b870d7708247291b53139f1c461e487
ChimichangasBoi <91523145+ChimichangasBoi@users.noreply.github.com>
2023-06-03 12:03:25 +0530

    Update ListQues.py

--- ListQues.py
+++ ListQues.py
@@ -10,14 +10,17 @@ Write function for following opeartions :
 #1. Swap any 2 elements of which indexes are passed eg 0, 4 --> 5,2,3,4,1
 #using 3rd variable
 
-def swapDeezNuts(l, pos1, pos2):
+def swapPos(l, pos1, pos2):
     temp=l[pos1]
     l[pos1]=l[pos2]
     l[pos2]=temp
     return l
 
-List = [5,4,3,2,1]
+k = [5,4,3,2,1]
 pos1= 1
-pos2=5
+pos2=3
+
+k.sort()
  
-print(swapDeezNuts(List, pos1-1, pos2-1))
+print(swapPos(k, pos1, pos2))
+
