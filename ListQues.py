11f0fe78d9afc06bf6c56a6b79f4ce3f9d8c8264
ChimichangasBoi <91523145+ChimichangasBoi@users.noreply.github.com>
2023-06-04 12:54:18 +0530

    Update ListQues.py

--- ListQues.py
+++ ListQues.py
@@ -6,3 +6,17 @@ Write function for following opeartions :
 3. Minimum value of that list --> 2
 4. Check if list is sorted in ascending order. --> True
 """
+
+def swapElement(l, pos1, pos2):
+    temp=l[pos1]
+    l[pos1]=l[pos2]
+    l[pos2]=temp
+    return l
+
+k = [5,4,3,2,1]
+pos1= 1
+pos2=3
+
+k.sort()
+ 
+print(swapElement(k, pos1, pos2))
