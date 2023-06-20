9d35765758ab5b8cb23f950ace1e6a313ab95980
ChimichangasBoi <91523145+ChimichangasBoi@users.noreply.github.com>
2023-06-20 20:54:21 +0530

    Update ListQues.py

--- ListQues.py
+++ ListQues.py
@@ -9,13 +9,14 @@ Write function for following opeartions :
 
 #did some changes from scratch as there were some indention error
 #sorting algo is not implementing correctly I'll do it tomorrow
+
 def swapElement(l, pos1, pos2):
     temp = l[pos1]
     l[pos1] = l[pos2]
     l[pos2] = temp
     return l
 
-k = [1,2,3,4,5]
+k = [5,4,3,2,1]
 pos1 = 1
 pos2 = 3
 
@@ -46,6 +47,13 @@ def Min(k):
 print("Largest element is:", Max(k))
 print("Smallest element is:", Min(k))
 
+for i in range(len(k)):
+    for j in range(i +1 , len(k)):
+
+        if k[i] > k[j]:
+            k[i], k[j] = k[j], k[i]
+
+print(k)
 
 
 
