e46be1c81a66c9dac1d0013c9898667ca64bfd4c
Tejas <77725958+tejas-k3@users.noreply.github.com>
2023-08-03 21:33:11 +0530

    Refactoring code

--- ListQues.py
+++ ListQues.py
@@ -7,8 +7,25 @@ Write function for following opeartions :
 4. Check if list is sorted in ascending order. --> True
 """
 
+# Creating a list having Elements 1,2,3,4,5
+myList = [1,2,3,7,33,5]
+# Swapping 2 indexes of which indexes are passed
+index1, index2 = 0, 4
+print(f"List before swapping at {index1},{index2} indexes is ", myList)
+swapElement(myList, index1, index2)
+print(f"List after swapping at {index1},{index2} indexes is ", myList)
+# Print maximum value of list
+print(Max(myList))
+# Print minimum value of list
+print(Min(myList))
+# Sorting the list in ascending order
+print("Original list order is ", myList)
+print("List after sorting in ASC order is ", myList)
+sortList(myList, "ASC")
+# Sorting the list in descending order
+sortList(myList, "DESC")
+print("List after sorting in DESC order is ", myList)
 
-#It returns same list with sorted values based on ASC or DESC value provided
 def sortList(lst, order): #bubble sorting
     if order == 'ASC': 
         for i in range(len(lst)): #lst refers to the list to be sorted
@@ -23,31 +40,15 @@ def sortList(lst, order): #bubble sorting
     else:
         print("error")
 
-
-#1. Swap any 2 elements of which indexes are passed eg 0, 4 --> 5,2,3,4,1
-#using 3rd variable
-
 def swapElement(l, pos1, pos2):
     temp = l[pos1]
     l[pos1] = l[pos2]
     l[pos2] = temp
     return l
 
-k = [5,4,3,2,1]
-pos1 = 1
-pos2 = 3
-
-
-print(swapElement(k, pos1, pos2))
-
-print("Sorted in ASC order:", sortList(k.copy(), 'ASC')) #copy() returns a copy of the specified list
-print("Sorted in DESC order:", sortList(k.copy(), 'DESC'))
-
 def Max(k):
     if not k:  
         print("List is empty")
-        
-
     max = k[0]
     for ele in k:
         if ele > max:
@@ -57,16 +58,8 @@ def Max(k):
 def Min(k):
     if not k:  
         print("List is empty")
-       
-
     min = k[0]
     for ele in k:
         if ele < min:
             min = ele
     return min
-
-print("Largest element is:", Max(k))
-print("Smallest element is:", Min(k))
-
-
-
