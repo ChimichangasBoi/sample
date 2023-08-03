34d95f0a83c71703ae9b0049036ae25fce8272bc
Tejas <77725958+tejas-k3@users.noreply.github.com>
2023-08-03 23:38:06 +0530

    Create SetQues.py

---
+++ SetQues.py
@@ -0,0 +1,7 @@
+"""
+1. Create two set, setA having elements 1,2,3,4,5 set B having elements 3,4,5,6,7.
+2. Create a method which takes input of 2 sets and returns elements which are common.
+3. Create a method which takes input of 2 sets and returns elements which are not common.
+4. Create a method which takes input of 2 sets and return a set having all elements from setA & setB.
+5. Create a method for takes input of a set & an element and returns True if that element is in that set else returns False.
+"""
